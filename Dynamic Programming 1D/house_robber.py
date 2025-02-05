79811f5db5796551c38b8b316860c82c3736bf7d
Yan <EYAN2@ford.com>
2025-02-05 15:32:21 -0800

    2nd attempt on house robber and palindromic substring

--- Dynamic Programming 1D/house_robber.py
+++ Dynamic Programming 1D/house_robber.py
@@ -1,16 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        max_accum = {}
-        def rob_solver(max_accum, nums, index):
-            if index == 0:
-                return nums[0]
-            if index == 1:
-                return max(nums[0], nums[1])
-            if index in max_accum:
-                return max_accum[index]
-                
-            max_accum[index] = max(rob_solver(max_accum, nums, index - 2) + nums[index], rob_solver(max_accum, nums, index - 1))
-            return max_accum[index]
-        return rob_solver(max_accum, nums, len(nums) - 1)
-    
+        memo = {}
+        def rob_solve(idx):
+            if idx >= len(nums):
+                return 0
+            if idx in memo:
+                return memo[idx]
 
+            # rob first house, then we can only rob the 3rd house now
+            rob_first = nums[idx] + rob_solve(idx + 2)
+            # dont rob first house, then we can only rob the 2nd house 
+            rob_second = rob_solve(idx + 1)
+
+            memo[idx] = max(rob_first, rob_second)
+            return memo[idx]
+        return rob_solve(0)
+        
--- Dynamic Programming 1D/longest_palindromic_substring.py
+++ Dynamic Programming 1D/longest_palindromic_substring.py
@@ -1,26 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pal = ""
-        palLen = 0
-        str_len = len(s)
-        for i in range(str_len):
-            l = i
-            r = i
-            while l >= 0 and r < str_len and s[l] == s[r]:
-                if (r - l + 1) > palLen:
-                    pal = s[l : r + 1]
-                    palLen = r - l + 1
-                l -= 1
-                r += 1
+        if len(s) == 1:
+            return s
+        longest = ""
+        # Returns the longest string length a given character
+        def expand(p1, p2) -> str:
+            while p1 >= 0 and p2 < len(s) and s[p1] == s[p2]:
+                p1 -= 1
+                p2 += 1
+            #add one to the left because we have reached -1,
+            #keep p2 the same because we have reached past the length
+            return s[p1+1:p2]
 
-            l = i
-            r = i + 1
-            while l >= 0 and r < str_len and s[l] == s[r]:
-                if (r - l + 1) > palLen:
-                    pal = s[l : r + 1]
-                    palLen = r - l + 1
-                l -= 1
-                r += 1
-        return pal
+        for i in range(len(s)):
+            pali = expand(i, i)
+            if len(pali) > len(longest):
+                longest = pali
+            pali = expand(i, i+1)
+            if len(pali) > len(longest):
+                longest = pali
+
+        return longest
+
+    
+        
 
 
+
+        
