01f438cbbd72cdb08575d552f8c57cc97fa5c808
Evan Yan <evanyans@gmail.com>
2025-02-10 10:53:05 -0800

    Create binary_search.py

---
+++ Binary Search/binary_search.py
@@ -0,0 +1,15 @@
+class Solution:
+  # Attempted 2/10/2025
+  # AHA: use pointers rather than recursion
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
