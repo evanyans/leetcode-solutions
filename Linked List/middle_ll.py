5792af1e87ff58c4aea12e7cb2ac3ed8579091d2
Evan Yan <evanyans@gmail.com>
2025-02-19 20:15:36 -0800

    Create middle_ll.py

---
+++ Linked List/middle_ll.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # Attempted 2/19/2025
+    # aha: just a fundamental: slow and fast pointer method
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
