bc3823e707c9602bf5fdf8d51a64047246285acd
Evan Yan <evanyans@gmail.com>
2025-03-12 15:26:13 -0700

    Update coin_change.py

--- Dynamic Programming 1D/coin_change.py
+++ Dynamic Programming 1D/coin_change.py
@@ -1,3 +1,24 @@
+class Solution:
+    # Attempted 3/12/2025
+    # AHA: still dont get it
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1) 
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for n in coins:
+                # i - n represents the remainder coins needed to form the current "amount"
+                if i - n >= 0:
+                    # - minimum of itself (check see if we already have better/worse previous solution)
+                    # - 1 + dp[i-c] represents us calculating dp of i.
+                    dp[i] = min(dp[i], 1 + dp[i - n])
+
+        if dp[amount] == amount + 1:
+            return -1
+        return dp[amount]
+
+        
+
 # Attempted 2/5/2025 for the first time
 # Aha moment: Bottom-up dynamic programming (focus on the smallest case of calculating coinChange for amount 0)
 class Solution:
