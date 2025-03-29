d7b4461b31559efca22885a0caa8e3c259f5d092
Evan Yan <evanyans@gmail.com>
2025-03-28 21:29:07 -0700

    Create subsets.py

---
+++ Backtracking/subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    # Attempted 3/28/2025
+    # AHA: backtracking template
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(curr, i):
+            if i > len(nums):
+                return
+            ans.append(curr[:])
+            for j in range(i, len(nums)):
+                curr.append(nums[j])
+                backtrack(curr, j + 1)
+                curr.pop()
+        ans = []
+        backtrack([], 0)
+        return ans
+
+
+    
