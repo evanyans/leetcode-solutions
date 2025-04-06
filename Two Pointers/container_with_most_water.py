b522ee0897e39879a6c8734a488de067450e0338
Evan Yan <evanyans@gmail.com>
2025-04-05 22:15:41 -0700

    Update container_with_most_water.py

--- Two Pointers/container_with_most_water.py
+++ Two Pointers/container_with_most_water.py
@@ -1,21 +1,24 @@
 class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        p1 = 0
-        p2 = len(heights) - 1
-        max_area = 0
-        temp_area = 0
-
-        while p1 < p2:
-            width = p2 - p1
-
-            temp_area = min(heights[p1], heights[p2]) * width
-            
-            if temp_area > max_area:
-                max_area = temp_area
-
-            if heights[p1] < heights[p2]:
-                p1 += 1
+    # ReAttempted 4/5/2025
+    # AHA: write out a test case, think about how to progress l and r
+    def maxArea(self, height: List[int]) -> int:
+        # observations: we start on outside for max width
+        l = 0
+        r = len(height) - 1
+        max_water = 0
+        while l < r:
+            # 1, 8, 6, 2, 5, 4, 8, 3, 7
+            # 0, 1, 2, 3, 4, 5, 6, 7, 8
+            # 8 - 1 = 7 * 7 = 49
+            water = min(height[l], height[r]) * (r - l)
+            max_water = max(max_water, water)
+            if height[l] < height[r]:
+                l += 1
             else:
-                p2 -= 1
+                r -= 1
+        return max_water
 
-        return max_area
+        
+
+
+            
