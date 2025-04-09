838da74d18bc13b9d3df18af32369b6bc905d3ea
Evan Yan <evanyans@gmail.com>
2025-04-08 22:05:44 -0700

    Update number_of_islands.py

--- Graphs/number_of_islands.py
+++ Graphs/number_of_islands.py
@@ -1,10 +1,10 @@
 class Solution:
-    # Attempted 3/23/2025
-    # AHA: very simple DFS, barebones matrix dfs
+    # Attempted 4/8/2025
+    # AHA: again vvery simple 
     def numIslands(self, grid: List[List[str]]) -> int:
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n and grid[row][col] == "1"
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
         def dfs(row, col):
             for dx, dy in directions:
                 next_row, next_col = row + dy, col + dx
@@ -14,12 +14,15 @@ class Solution:
 
         m = len(grid)
         n = len(grid[0])
-        ans = 0
         seen = set()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        ans = 0
+        
         for row in range(m):
             for col in range(n):
-                if grid[row][col] == "1" and (row, col) not in seen:
+                if grid[row][col] == "1" and (row,col) not in seen:
                     ans += 1
                     seen.add((row, col))
-                    dfs(row, col)
+                    dfs(row,col)
+
         return ans
