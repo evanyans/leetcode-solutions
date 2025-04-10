9474f496444802c00de97014e6f905fcee923f4e
Evan Yan <evanyans@gmail.com>
2025-04-09 23:22:22 -0700

    Update top_k_frequent.py

--- Heaps/top_k_frequent.py
+++ Heaps/top_k_frequent.py
@@ -1,4 +1,17 @@
 class Solution:
+    # reattempted 4/9/2025
+    # AHA: basic heap template
+    class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        nums_count = Counter(nums)
+        for key, val in nums_count.items():
+            # heap is minheap by default, i.e. we will pop smallest
+            heapq.heappush(heap, (val, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [pair[1] for pair in heap]
     # Attempted 3/29/2025
     # AHA: when we do a minheap, we pop smallest elements, reemember htat
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
