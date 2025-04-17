ef87c4b63801318457aa54d03565f0fb3f25ca64
Evan Yan <evanyans@gmail.com>
2025-04-16 20:59:16 -0700

    Update reverse_linked_list.py

--- Linked List/reverse_linked_list.py
+++ Linked List/reverse_linked_list.py
@@ -3,17 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
+    # REDO 4/16/2025
+    # AHA: WE RETURN THE END OF THE LL WHICH IS BEFORE, since it is the front now
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        before = None
-        after = None
         temp = head
+        before = after = None
         while temp:
             after = temp.next
             temp.next = before
             before = temp
             temp = after
-
         return before
-            
+           
+
